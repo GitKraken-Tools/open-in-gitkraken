190179b782d881af5a5666b97cdcc999d7233c3e
Justin White <kyjus25@gmail.com>
2021-11-05 15:27:18 -0500

    Attempt to get first commit, stops at 100

--- gh/content.js
+++ gh/content.js
@@ -1,13 +1,26 @@
+
+
+
 $( document ).ready(function() {
-    const modal = $("div").find(`[data-target='get-repo.modal']`).find('ul');
-    const repo = modal.find(' > li:first-of-type input[data-autoselect]')[0].value;
-    console.log(repo);
-    modal.find(' > li:nth-last-child(1)').before(link(repo));
+    render();
 });
 
-const link = (repo) => {
+async function render() {
+    const modal = $("div").find(`[data-target='get-repo.modal']`).find('ul');
+    const repoUrl = modal.find(' > li:first-of-type input[data-autoselect]')[0].value;
+    const user = repoUrl.split('/')[3];
+    const repo = repoUrl.split('/')[4].replace('.git', '');
+    const commits = await axios(`https://api.github.com/repos/${user}/${repo}/commits?per_page=9999999`).then(i => i.data);
+    const sha = commits[commits.length - 1].sha;
+    console.log('commits', commits);
+    if (commits && commits.length >= 1) {
+        modal.find(' > li:nth-last-child(1)').before(link(repoUrl, sha));
+    }
+}
+
+const link = (repo, sha) => {
     return `<li class="Box-row Box-row--hover-gray p-3 mt-0 rounded-0"> \
-        <a target="_blank" href="gitkraken://repolink/1?url=${repo}" class="d-flex flex-items-center color-text-primary text-bold no-underline"> \
+        <a target="_blank" href="gitkraken://repolink/${sha}?url=${repo}" class="d-flex flex-items-center color-text-primary text-bold no-underline"> \
             <img style="margin-right: 5px;" width="18" height="18" src="${chrome.extension.getURL('assets/keif-dark.png')}"> \
             Open in GitKraken \
         </a> \
